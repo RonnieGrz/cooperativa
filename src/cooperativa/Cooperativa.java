f52ed728b23e8ae9751ca784cd3ab58808a4fe04
valer <valer@Valeria>
2023-11-14 14:59:34 -0500

    puse un mensaje en cooperativa

--- src/cooperativa/Cooperativa.java
+++ src/cooperativa/Cooperativa.java
@@ -14,6 +14,7 @@ public class Cooperativa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("holaaaa");
         // TODO code application logic here
     }
     
