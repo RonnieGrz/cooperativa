4db5ca1e7647c769db35f5f5049801732a674b28
LauraSofi05 <laubotina@unicaca.edu.co>
2023-11-14 15:35:12 -0500

    hola

--- src/cooperativa/Cooperativa.java
+++ src/cooperativa/Cooperativa.java
@@ -14,7 +14,7 @@ public class Cooperativa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("holaaaa");
+        System.out.println("Cope");
         // TODO code application logic here
     }
     
